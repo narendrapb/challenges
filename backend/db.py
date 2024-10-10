7fd59a778421f24d5ee5cd6815fdd046c9167547
narendra_gitlab <narendra.boyina@vectramind.com>
2024-10-10 15:59:49 +0530

    added a route

--- backend/db.py
+++ backend/db.py
@@ -1,29 +1,13 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import select
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
 from sqlalchemy import (
     create_engine,
     Column,
     Integer,
     String,
-    DateTime,
-    ForeignKey,
-    Boolean,
-    VARBINARY,
-    Date,
-    JSON,
-    Text,
-    Float,
-    UniqueConstraint,
 )
 
-def createconnection():
-    try:
-        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        return session
-    except Exception as e:
-        print("error in create connection-----", e)
+mysql_con= "mysql+mysqlconnector://narendb:Admin123$@narendb.mysql.database.azure.com:3306/narendatabase"
 
 class Base(DeclarativeBase):
     pass
@@ -31,8 +15,36 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "User_account"
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(30))
+    email = Column(String(30))
     password = Column(String(30))
 
-def user_add(username,passwd):
-    pass
+engine = create_engine(mysql_con, echo=True)
+Base.metadata.create_all(engine) 
+
+def createconnection():
+    try:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
+    except Exception as e:
+        print("error in create connection-----", e)
+
+def user_add(mail,passwd):
+    try:
+        session = createconnection()
+        stmt=select(User).where(User.email==mail)
+        result = session.execute(stmt).first()
+        if result:
+            return (f"email: {mail} is alredy present try with other mail")
+        else:
+            user_data=User(email=mail,password=passwd)
+            session.add(user_data)
+            session.commit()
+            session.close()
+            return "User created Succussfully"
+    except Exception as e:
+        return ("error in creat accout", str(e))
+
+print(user_add("narend@gmail.com","Rowdy@143"))
+
+

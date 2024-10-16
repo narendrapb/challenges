399b2acf2efb80ba681d430bd38da7be0868d2b2
narendra_gitlab <narendra.boyina@vectramind.com>
2024-10-16 15:17:58 +0530

    added

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db import *
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this in production to your front-end origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_app_description():
   return (" i am Narendra Trying to build an application that having Frontend, Backend and Database")
